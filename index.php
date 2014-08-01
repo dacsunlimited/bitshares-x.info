8cbee2b1e2f2d45b330efe1a4c0bec70b349614c
root <root@bitshares-x.info>
2014-07-31 20:21:29 -0400

    update version

--- index.php
+++ index.php
@@ -76,8 +76,8 @@
             						     		</div><!-- end .modal-header -->
             						      		<div class="modal-body">
                                            						      			<div class="row">
-            						      			<a href="https://github.com/dacsunlimited/bitsharesx/releases/download/0.2.1/BitSharesX-0.2.1.exe" class=" whoabtn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
-            						      			<a href="https://github.com/dacsunlimited/bitsharesx/releases/download/0.2.1/BitSharesX-0.2.1.dmg" class="whoabtn" >BitsharesX for MAC</a>
+            						      			<a href="https://github.com/dacsunlimited/bitsharesx/releases/download/0.3.0/BitSharesX-0.3.0.exe" class=" whoabtn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
+            						      			<a href="https://github.com/dacsunlimited/bitsharesx/releases/download/0.3.0/BitSharesX-0.3.0.dmg" class="whoabtn" >BitsharesX for MAC</a>
             						      			           </div> 						      		</div><!-- end .modal-body -->
                                         				 <a style=" float: none;"  class="close" data-dismiss="modal" data-target="#myModal"><i class="icon-eye-off"></i> close</a>
   
