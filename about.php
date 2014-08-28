a22374a9a39de80f3212b2d9b92b0570c75253aa
bitsha256 <bitsha256@gmail.com>
2014-08-28 13:37:05 -0400

    update problem version

--- about.php
+++ about.php
@@ -134,7 +134,7 @@ Join us this Thursday, Aug 28th for a free webinar!</p>
 							possible with BitSharesX.  Your
 							one stop shop for cryptoequities
 							and bitassets. 	</p>
-							<a href="/files/wallet/0.4.9/BitSharesX-0.4.9.exe" class="pull-left whoabtn btn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
+							<a href="/files/wallet/0.4.9/BitSharesX-0.4.9-a.exe" class="pull-left whoabtn btn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
 							<a href="/files/wallet/0.4.9/BitSharesX-0.4.9.dmg" class="pull-left whoabtn btn" >BitsharesX for MAC</a>
 							
 												
