97c85478f6a1dc5b665c1075ba8d8672890f81de
bitsha256 <bitsha256@gmail.com>
2014-09-05 08:56:27 +0800

    version 0.2.12

--- about.php
+++ about.php
@@ -128,8 +128,8 @@
 							possible with BitSharesX.  Your
 							one stop shop for cryptoequities
 							and bitassets. 	</p>
-							<a href="/files/wallet/0.4.10/BitSharesX-0.4.10.exe" class="pull-left whoabtn btn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
-							<a href="/files/wallet/0.4.10/BitSharesX-0.4.10.dmg" class="pull-left whoabtn btn" >BitsharesX for MAC</a>
+							<a href="/files/wallet/0.4.12/BitSharesX-0.4.12.exe" class="pull-left whoabtn btn" style="margin: 0 24px 12px 0;">BitsharesX for Windows</a>
+							<a href="/files/wallet/0.4.12/BitSharesX-0.4.12.dmg" class="pull-left whoabtn btn" >BitsharesX for MAC</a>
 							
 												
 					</div><!-- end .aboutUsContent -->
