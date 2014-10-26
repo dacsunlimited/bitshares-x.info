aac3ba4d23d88b686bb4eba17dae7f56d93fcd0b
cass <cassiopaia@riseup.net>
2014-10-26 21:23:01 +0100

    Forcing https

--- partials/header.php
+++ partials/header.php
@@ -44,3 +44,17 @@ if (stripos($_SERVER['REQUEST_URI'], 'about')){
 } else {
  echo ' <body class="home" data-spy="scroll" data-target=".navbar" data-offset="53">';
 }
+
+
+
+// Force HTTPS for security
+if($_SERVER["HTTPS"] != "on") {
+ $pageURL = "Location: https://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+ }
+ header($pageURL);
+}
+
